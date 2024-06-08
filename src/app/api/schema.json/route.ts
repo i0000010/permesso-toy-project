8dc74ba929284e372de8d6bfb9cdabaf4b1b1f3d
Mark <markmaceachen@live.com>
2024-06-07 21:09:01 -0600

    openapi codegen

--- src/app/api/schema.json/route.ts
+++ src/app/api/schema.json/route.ts
@@ -8,7 +8,99 @@ export function GET(req: NextRequest, res: NextResponse) {
             title: 'permesso',
             version: '1.0.0',
         },
-        // servers:
+        servers: [
+            {
+                url: `${process.env.NEXT_PUBLIC_API_URL}/api/schema.json`,
+                description: 'Development server',
+            }
+        ],
+        paths: {
+            '/vectors/upsert': {
+                post: {
+                    summary: 'Upsert a vector',
+                    description: 'Upsert a vector',
+                    requestBody: {
+                        required: true,
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/EventSchema',
+                                },
+                            },
+                        },
+                    },
+                    responses: {
+                        200: {
+                            description: 'Successful response',
+                        },
+                        400: {
+                            description: 'Bad request',
+                        },
+                    },
+                },
+            },
+            '/vectors/query': {
+                post: {
+                    summary: 'Query a vector',
+                    description: 'Query a vector',
+                    requestBody: {
+                        required: true,
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/QuerySchema',
+                                },
+                            },
+                        },
+                    },
+                    responses: {
+                        200: {
+                            description: 'Successful response',
+                        },
+                        400: {
+                            description: 'Bad request',
+                        },
+                    },
+                },
+            },
+        },
+        components: {
+            schemas: {
+                EventSchema: {
+                    type: 'object',
+                    properties: {
+                        event: {
+                            type: 'object',
+                            properties: {
+                                data: {
+                                    type: 'object',
+                                    properties: {
+                                        id: {
+                                            type: 'string',
+                                        },
+                                        body: {
+                                            type: 'string',
+                                        },
+                                    },
+                                    required: ['id', 'body'],
+                                },
+                            },
+                            required: ['data'],
+                        },
+                    },
+                    required: ['event'],
+                },
+                QuerySchema: {
+                    type: 'object',
+                    properties: {
+                        content: {
+                            type: 'string',
+                        },
+                    },
+                    required: ['content'],
+                },
+            },
+        }
         
     };
     return NextResponse.json(schema);
